de4457ef6621c81d2156831bb576971ef80b0ac4
James Bourne <james.bourne@infinityworks.com>
2019-10-04 16:47:04 +0100

    Query from client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import ApolloClient, { gql } from "apollo-boost";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  uri: "https://9vkow8dtoe.execute-api.eu-west-2.amazonaws.com/dev/graphql"
+});
 
 // Create a <Title> react component that renders an <h1> which is
 // centered, palevioletred and sized at 1.5em
@@ -16,12 +22,45 @@ const Wrapper = styled.section`
   background: papayawhip;
 `;
 
+const BodyWrapper = styled.section`
+  padding: 4em;
+`;
+
+const PLANTS = gql`
+  {
+    getPlants {
+      id
+      name
+    }
+  }
+`;
+
 const App: React.FC = () => {
   return (
-    <Wrapper>
-      <Title>Plant Watering!</Title>
-    </Wrapper>
+    <ApolloProvider client={client}>
+      <Wrapper>
+        <Title>Plant Watering!</Title>
+      </Wrapper>
+      <BodyWrapper>
+        <Plants />
+      </BodyWrapper>
+    </ApolloProvider>
   );
+};
+
+const Plants: React.FC = () => {
+  const { loading, error, data } = useQuery(PLANTS);
+
+  if (!data) {
+    return <>Loading...</>;
+  }
+
+  return data.getPlants.map((p: any) => (
+    <>
+      <h1>{p.name}</h1>
+      <p>{p.id}</p>
+    </>
+  ));
 };
 
 export default App;
